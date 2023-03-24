188ed67439e86b3f8716b8c63215e35ed349be0f
Rodrigo Andre Gutierrez <rodrigoandregutierrez@gmail.com>
2023-03-24 04:04:01 -0300

    se corrige posicion de index.jsx en carpeta raiz

--- index.jsx
+++ index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { App } from './src/App';
 import './index.scss';
-import { Provider } from './Provider/Provider';
+import { Provider } from './src/Provider/Provider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
